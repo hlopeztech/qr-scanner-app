08c188456d5fc2a3a4da9f55ddcf13c1d6435a58
hlopezdev <hald.ddlh@hotmail.com>
2023-04-14 18:41:13 -0400

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("No escaneado aun");
+
+  const askForCameraPermission = async () => {
+    const { status } = await BarCodeScanner.requestPermissionsAsync();
+    setHasPermission(status === "granted");
+  };
+
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log(
+      `Bar code with type ${type} and data ${data} has been scanned!`
+    );
+  };
+
+  if (hasPermission === null) {
+    return <Text>Solicitando permiso de la camara</Text>;
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ color: "#fff" }}>
+          No hay acceso a la camara, por favor habilita el permiso
+        </Text>
+        <Button title={"Reintentar"} onPress={() => askForCameraPermission()} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 300, width: 300 }}
+        />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+      {scanned && (
+        <Button title={"Escanear de nuevo"} onPress={() => setScanned(false)} color='tomato' />
+      )}
     </View>
   );
 }
@@ -13,8 +59,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  barcodebox: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: 300,
+    height: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: "tomato",
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
   },
 });
